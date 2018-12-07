48b8e75c8426c9074b182ecae15d197cd40b6ede
bernagl <luisb.galo@gmail.com>
2018-12-07 13:32:02 -0600

    Init

--- README.md
+++ README.md
@@ -1,31 +1,68 @@
 # react-luxbar
 
-> 
+>
 
 [![NPM](https://img.shields.io/npm/v/react-luxbar.svg)](https://www.npmjs.com/package/react-luxbar) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
-## Install
+###Description
 
-```bash
-npm install --save react-luxbar
-```
+A [Luxbar](https://balzss.github.io/luxbar/ 'Luxbar') implementation for React.js
 
-## Usage
+###Installation
+`yarn add react-luxbar` -or- `npm install --save react-luxbar`
+
+###Demo
+[Demo](https://build-ttrkyrbbjw.now.sh/ 'Demo')
+
+###API
+####Navbar
+| Prop | Type |Default|Options|Description|
+| ------------ | ------------ |
+| backgroundColor | String:Color |||Navbar background color|
+| className | String |||Custom classNames for Navbar|
+|color|String:Color|white||Navbar color text|
+|hoverColor|String:Color|white||Navbar items hover color|
+|onToggle|Function(status)|||Callback for navbar toggles|
+|position|string|fixed|'fixed', 'fixedBottom', 'static'|Navbar position|
+|rtl|Bool|true|true, false|Options right to left|
+
+####Navbar.Option
+| Prop | Type |Default|Options|Description|
+| ------------ | ------------ |
+| className | String |||Custom classNames for Navbar.Option|
+
+####Navbar.Logo
+| Prop | Type |Default|Options|Description|
+| ------------ | ------------ |
+| className | String |||Custom classNames for Navbar.Option|
+
+###Usage
 
 ```jsx
 import React, { Component } from 'react'
-
-import MyComponent from 'react-luxbar'
+import Navbar from 'react-luxbar'
 
 class Example extends Component {
-  render () {
+  render() {
     return (
-      <MyComponent />
+      <Navbar>
+        <Navbar.Logo>
+          <a href="index.html">
+            <span>Brand</span>
+          </a>
+        </Navbar.Logo>
+        <Navbar.Option>
+          <a href="https://google.com">Google</a>
+        </Navbar.Option>
+      </Navbar>
     )
   }
 }
 ```
 
-## License
+###Credits
+[Luxbar](https://balzss.github.io/luxbar/ 'Luxbar')
+
+### License
 
 MIT © [bernagl](https://github.com/bernagl)
--- src/index.js
+++ src/index.js
@@ -1,26 +1,135 @@
-/**
- * @class ExampleComponent
- */
-
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
-import styles from './styles.css'
+const NavbarContext = React.createContext()
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+const NavbarConsumer = props => {
+  class Hover extends React.Component {
+    state = { hover: false }
+
+    toggleHover = hover => this.setState({ hover })
+
+    opts = () => ({ toggleHover: this.toggleHover, hover: this.state.hover })
+
+    render() {
+      return (
+        <NavbarContext.Consumer {...props}>
+          {context => {
+            if (!context)
+              throw new Error(
+                'Navbar compound components cannot be rendered outside the Navbar component'
+              )
+            return props.children({ ...context, ...this.opts() })
+          }}
+        </NavbarContext.Consumer>
+      )
+    }
   }
 
-  render() {
-    const {
-      text
-    } = this.props
+  return <Hover />
+}
 
+class Navbar extends React.Component {
+  state = { on: false, positions: ['fixed', 'fixedBottom', 'static'] }
+
+  static defaultProps = {
+    backgroundColor: '#00dbe7',
+    color: 'white',
+    className: '',
+    hoverColor: '#00dbe7',
+    position: 'fixed',
+    onToggle: on => console.log(`toggle to: ${on ? 'on' : 'off'}`),
+    rtl: true
+  }
+
+  static Logo = ({ className, children }) => (
+    <NavbarConsumer>
+      {({ rtl, toggle }) => (
+        <li
+          className={`luxbar-header ${rtl ? 'rtl' : 'ltr'} ${className || ''}`}
+        >
+          <span className="luxbar-brand">{children}</span>
+          <label
+            className="luxbar-hamburger luxbar-hamburger-doublespin"
+            id="luxbar-hamburger"
+            htmlFor="luxbar-checkbox"
+            onClick={toggle}
+          >
+            <span />
+          </label>
+        </li>
+      )}
+    </NavbarConsumer>
+  )
+
+  static Option = ({ className, children }) => (
+    <NavbarConsumer>
+      {({ hover, hoverColor, rtl, toggle, toggleHover }) => (
+        <li
+          className={`luxbar-item ${rtl ? 'rtl' : 'ltr'} ${className} ${hover ? 'hover' : ''}`}
+          style={
+            hover ? { backgroundColor: hoverColor } : null
+          }
+          onClick={toggle}
+          onMouseEnter={() => toggleHover(true)}
+          onMouseLeave={() => toggleHover(false)}
+        >
+          {children}
+        </li>
+      )}
+    </NavbarConsumer>
+  )
+
+  toggle = () =>
+    this.setState(
+      ({ on }) => ({ on: !on }),
+      () => this.props.onToggle(this.state.on)
+    )
+
+  options = () => ({
+    on: this.state.on,
+    error: this.error,
+    hoverColor: this.props.hoverColor,
+    rtl: this.props.rtl,
+    toggle: this.toggle
+  })
+
+  render() {
+    const { on, positions } = this.state
+    const { backgroundColor, className, color, position, rtl } = this.props
+    const pos = positions.includes(position) ? position : 'fixed'
     return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
+      <NavbarContext.Provider value={this.options()}>
+        <header id="luxbar" className={`luxbar-${pos}`}>
+          <input
+            type="checkbox"
+            className="luxbar-checkbox"
+            id="luxbar-checkbox"
+            checked={on}
+            onChange={() => null}
+          />
+          <div
+            className={`luxbar-menu luxbar-menu-right luxbar-menu-material-cyan ${className}`}
+            style={{ backgroundColor, color }}
+          >
+            <ul className={`luxbar-navigation ${rtl ? 'rtl' : 'ltr'}`}>
+              {this.props.children}
+            </ul>
+          </div>
+        </header>
+      </NavbarContext.Provider>
     )
   }
 }
+
+Navbar.propTypes = {
+  backgroundColor: PropTypes.string,
+  className: PropTypes.string,
+  color: PropTypes.string,
+  hoverColor: PropTypes.string,
+  onToggle: PropTypes.func,
+  position: PropTypes.oneOf(['fixed', 'fixedBottom', 'static']),
+  rtl: PropTypes.bool
+}
+
+export default Navbar
